b33f317089b3758dc6733d0e376e135f94cd8d3f
DIS-Wolv <thomas.lieumont@gmail.com>
2025-03-10 15:28:35 +0100

    nouveaux logo

--- mod.cpp
+++ mod.cpp
@@ -1,7 +1,7 @@
 name			= "DIS-Utils";				// Name of your mod
 author			= "Wolv";					// Affects Arma 3 Launcher, when the mod are loaded as local
-logo			= "dis.jpg";			// Logo displayed in the main menu
-logoOver		= "dis.jpg";	// When the mouse is over, in the main menu
+logo			= "DIS.paa";			// Logo displayed in the main menu
+logoOver		= "DIS.paa";	// When the mouse is over, in the main menu
 // tooltip			= "mod_";
 // tooltipOwned	= "credit_mod";				// Tool tip displayed when the mouse is left over, in the main menu
 picture			= "disMoto.jpg";		// Picture displayed from the expansions menu. Optimal size is 2048x1024
@@ -11,4 +11,4 @@ overview		= "Mod DIS";	// Supports structured text
 hideName		= 0;						// Hide the extension name
 hidePicture		= 0;						// Hide the extension menu
 dlcColor[]		= { 0.23, 0.39, 0.30, 1 };	// Color used for DLC stripes and backgrounds (RGBA)
-logoSmall		= "dis.jpg";		// Display in creative lists, next to the entities added by the mod
+logoSmall		= "DIS.paa";		// Display in creative lists, next to the entities added by the mod
