e4932cba2309f6032310155e3060e95f573d378b
DIS-Wolv <thomas.lieumont@gmail.com>
2025-05-18 11:27:50 +0200

    path test 2

--- addons/WOLV_Garage/includeMaster.hpp
+++ addons/WOLV_Garage/includeMaster.hpp
@@ -1,4 +1,4 @@
 #include "GUI\DefineA3.hpp"
-#include "GUI\WOLV_GUIGarage_Garage.hpp" // 
+#include "GUI\WOLV_GUIGarage_Garage.hpp"
 #include "GUI\WOLV_GUIGarage_Inv.hpp"
 #include "GUI\WOLV_GUIGarage_InvAce.hpp"
